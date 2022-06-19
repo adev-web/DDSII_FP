217ab0cd10df4e649ba173c77c8c7d33a29e0b8c
NOVA945 <80370055+NOVA945@users.noreply.github.com>
2022-06-18 22:42:05 -0500

    intento1

--- src/proyecto1/PROYECTO1.java
+++ src/proyecto1/PROYECTO1.java
@@ -1,12 +1,15 @@
 
 package proyecto1;
 
+import FORMULARIOS.FRMPRINCIPAL;
+
 
 public class PROYECTO1 {
 
    
     public static void main(String[] args) {
-    
+        FRMPRINCIPAL FRM=new FRMPRINCIPAL();
+        FRM.setVisible(true);
     }
     
 }
